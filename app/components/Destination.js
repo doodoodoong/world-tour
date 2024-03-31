b1465025ed7e6b9fd705f508e11a996996c1535b
doodoodoong <doo_doong@hanmail.net>
2024-03-31 11:11:27 +0900

    card

---
+++ app/components/Destination.js
@@ -0,0 +1,11 @@
+"use client";
+
+import { Box, Text } from "@chakra-ui/react";
+
+export default function Destination() {
+  return (
+    <Box marginX={"63px"} marginTop={"96px"} marginBottom={"16px"}>
+      <Text>가까운 나라</Text>
+    </Box>
+  );
+}
--- app/pages/Main.js
+++ app/pages/Main.js
@@ -2,11 +2,13 @@
 
 import { Box } from "@chakra-ui/react";
 import Welcome from "../components/Welcome";
+import Destination from "../components/Destination";
 
 export default function Main() {
   return (
     <Box>
       <Welcome />
+      <Destination />
     </Box>
   );
 }
