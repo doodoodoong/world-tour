8a79334ca504017c527db43d412a88b4e59fc925
doodoodoong <doo_doong@hanmail.net>
2024-03-31 11:42:23 +0900

    card component ing

---
+++ app/components/Card.js
@@ -0,0 +1,36 @@
+"use client";
+
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import PropTypes from "prop-types";
+
+CardPage.propTypes = {
+  country: PropTypes.string.isRequired,
+  imgSrc: PropTypes.string.isRequired,
+  detail: PropTypes.string.isRequired,
+};
+
+export default function CardPage({ country, imgSrc, detail }) {
+  return (
+    <Card width={"316px"} height={"352px"}>
+      <CardBody backgroundImage={imgSrc} bgSize={"cover"}>
+        <Flex flexDirection={"column"} justifyContent={"space-between"}>
+          <Heading size={"md"}>{country}</Heading>
+          <Text>{detail}</Text>
+          <Button variant={"ghost"} backgroundColor={"white"}>
+            Explore
+          </Button>
+        </Flex>
+      </CardBody>
+    </Card>
+  );
+}
--- app/components/Destination.js
+++ app/components/Destination.js
@@ -1,11 +1,21 @@
 "use client";
 
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import CardPage from "./Card";
 
 export default function Destination() {
   return (
-    <Box marginX={"63px"} marginTop={"96px"} marginBottom={"16px"}>
-      <Text>가까운 나라</Text>
-    </Box>
+    <>
+      <Box marginX={"63px"} marginTop={"96px"} marginBottom={"16px"}>
+        <Text>가까운 나라</Text>
+      </Box>
+      <Flex>
+        <CardPage
+          country="France"
+          imgSrc="http://unsplash.it/2000?random&gravity=center"
+          detail="Lorem occaecat aliquip et esse consequat cillum irure mollit."
+        />
+      </Flex>
+    </>
   );
 }
